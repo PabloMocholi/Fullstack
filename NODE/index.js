18145a887c4598c1cf508c7c223a15455b7dd155
PabloMocholi <148113056+PabloMocholi@users.noreply.github.com>
2024-01-29 12:02:00 +0100

    Jugando con node

---
+++ NODE/index.js
@@ -0,0 +1,8 @@
+console.clear();
+
+console.log("Bienvenidos a mi primer archivo NODE.JS!!");
+
+const nombre = "Tomi";
+
+//nombre.addEventListener... da error porque estos eventos vienen de la vista
+console.log(nombre);
